bda27154bbf3b3df184f37d62bad3e269929a385
Bercik1337 <bercik1337@gmail.com>
2017-08-19 16:40:17 +0200

    mc colors

--- 01-prep-user.sh
+++ 01-prep-user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Copying personal configuration to home directory"
-cp -vr .bash_profile  .bashrc .nanorc .vimrc skrypty .bin ~/
+cp -vr .bash_profile  .bashrc .nanorc .vimrc skrypty .bin .config ~/
 . ~/.bashrc

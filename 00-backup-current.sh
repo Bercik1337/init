fec8612f50348a7dcf63665e3d187d454c72ff12
Bercik1337 <bercik1337@github.com>
2019-01-31 11:07:34 +0100

    added TOINSTALL

--- 00-backup-current.sh
+++ 00-backup-current.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Creating backup of current settings"
-tar cvf ~/backup.tar .bash_profile  .bashrc .nanorc .vimrc
+tar cvf ~/backup.tar .bash_profile  .bashrc .nanorc .vimrc .tmux_bootstrap .tmux.conf

4d3c9cbb52560982693f4807c0c42f46e80a3132
Bartłomiej Kacprzak <bercik@hubble.universe>
2019-03-09 11:57:24 +0100

    DR fix

--- root/skrypty/disasterrecovery.sh
+++ root/skrypty/disasterrecovery.sh
@@ -11,33 +11,33 @@ LOGFILE=/var/log/backupy
 TARGET=/home/bercik/torrenty/swap/dr
 
 echo "#################################################" >> $LOGFILE
-if [ `df | awk {'print $4'} | sed -n 6p` -gt 20000000 ]; then
+if [ `df /mnt/hgfs | awk {'print $4'} | sed -n 2p` -gt 20000000 ]; then
       echo "$STARTTIME - Duzo miejsca. Rozpoczynam tworzenie Disaster Recovery" | tee >> $LOGFILE
-        echo "Rozpoczeto tworzenie DisasterRecovery. Potrwa to okolo 30min" | wall
+        echo "Rozpoczeto tworzenie DisasterRecovery. Potrwa to okolo 5min" | wall
 	time dd if=/dev/sda of=$TARGET/hubble-sda-$DATE.img bs=1M 2>> $LOGFILE
         if [ $? -eq 0 ]; then
         echo -ne "Ok $(date '+%Y-%m-%d %H:%M:%S')\n" | tee >>  $LOGFILE
         else
-        echo -ne "Co¶ posz³o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
+        echo -ne "CoÅ› poszÅ‚o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
         fi
 else
-   echo -ne "Ma³o miejsca \n" | tee >> $LOGFILE
+   echo -ne "MaÅ‚o miejsca \n" | tee >> $LOGFILE
 fi
 echo "$STARTTIME - Wyliczam sume kontrolna" | tee >> $LOGFILE
 cd $TARGET
-time cfv -C -t sfv -f hubble-sda-$DATE.sfv hubble-sda-$DATE.img | tee >> $LOGFILE
+time rhash --percents --speed --sfv --output=hubble-sda-$DATE.img.sfv hubble-sda-$DATE.img >> $LOGFILE 
         if [ $? -eq 0 ]; then
-	cat $TARGET/hubble-sda-$DATE.sfv >> $LOGFILE
+	cat $TARGET/hubble-sda-$DATE.img.sfv >> $LOGFILE
         echo -ne "SFV - Ok $(date '+%Y-%m-%d %H:%M:%S')\n" | tee >>  $LOGFILE
         else
-        echo -ne "SFV - Co¶ posz³o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
+        echo -ne "SFV - CoÅ› poszÅ‚o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
         fi
-time cfv -C -t md5 -f hubble-sda-$DATE.md5 hubble-sda-$DATE.img| tee >> $LOGFILE
+time rhash --percents --speed -M --output=hubble-sda-$DATE.img.md5 hubble-sda-$DATE.img >> $LOGFILE
         if [ $? -eq 0 ]; then
-	cat $TARGET/hubble-sda-$DATE.md5 >> $LOGFILE
-        echo -ne "MD5 - Ok $(date '+%Y-%m-%d %H:%M:%S')\n" | tee >>  $LOGFILE
+        cat $TARGET/hubble-sda-$DATE.img.md5 >> $LOGFILE
+        echo -ne "SFV - Ok $(date '+%Y-%m-%d %H:%M:%S')\n" | tee >>  $LOGFILE
         else
-        echo -ne "MD5 - Co¶ posz³o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
+        echo -ne "SFV - CoÅ› poszÅ‚o nie tak $(date '+%Y-%m-%d %H:%M:%S') \n" | tee >> $LOGFILE
         fi
 echo "#################################################" >> $LOGFILE
 exit 0

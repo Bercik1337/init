5e182f251b45359becb8e8e3f173322aca04c514
Bercik1337 <bercik@hubble.universe>
2019-03-30 12:31:13 +0100

    01 install fix nano colors

--- 01-prep-user.sh
+++ 01-prep-user.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Copying personal configuration to home directory"
 cp -vr .bash_profile  .bashrc .nanorc .vimrc skrypty .bin .config .screenrc .selected_editor .bash-git-prompt ~/
+cp -vr .config/.nano ~/.config/
 . ~/.bashrc

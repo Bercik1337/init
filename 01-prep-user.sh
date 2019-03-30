352420f6aaafe70f2e4359e62170ffdebd5bace9
Bercik1337 <bercik@hubble.universe>
2019-03-30 10:49:49 +0100

    install 01 fix

--- 01-prep-user.sh
+++ 01-prep-user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Copying personal configuration to home directory"
-cp -vr .bash_profile  .bashrc .nanorc .vimrc skrypty .bin .config .screenrc .selected_editor ~/
+cp -vr .bash_profile  .bashrc .nanorc .vimrc skrypty .bin .config .screenrc .selected_editor .bash-git-prompt ~/
 . ~/.bashrc
